d510fb06cc6c6b4625738106f091cca0c1ea7b83
erlanggardr <erlanggarizqidwi@gmail.com>
2025-09-25 23:16:34 +0700

    Update photo gallery assets and add new images for navigation

--- quiz1/assets.js
+++ quiz1/assets.js
@@ -1,8 +1,8 @@
 // next prev buttons for photo galleries
 const galleries = {
   papuma: ["../assets/papuma.png", "../assets/papuma1.png", "../assets/papuma2.png"],
-  gambir: ["../assets/kebun-teh-gambir.png", "../assets/gambir1.jpg", "../assets/gambir2.jpg"],
-  puslitkoka: ["../assets/puslitkoka.png", "../assets/puslitkoka1.jpg", "../assets/puslitkoka2.jpg"]
+  gambir: ["../assets/kebun-teh-gambir.png", "../assets/kebun-teh-gambir1.png", "../assets/kebun-teh-gambir2.png"],
+  puslitkoka: ["../assets/puslitkoka.png", "../assets/puslitkoka1.png", "../assets/puslitkoka2.png", "../assets/puslitkoka3.png"]
 };
 
 const indices = { papuma: 0, gambir: 0, puslitkoka: 0 };
